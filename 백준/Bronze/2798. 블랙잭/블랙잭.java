2bc0bd470c15d3baf98319c6e1b33c90112d8a46
suhyeon0513 <90120036+suhyeon0513@users.noreply.github.com>
2022-09-08 13:42:51 +0900

    [Bronze II] Title: 블랙잭, Time: 304 ms, Memory: 24572 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        ArrayList<int[]> list = new ArrayList<>();
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+loop:   for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                for (int k = j+1; k < N; k++) {
+                    sum = arr[i] + arr[j] + arr[k];
+                    if(sum <= M)
+                        list.add(new int[] {Math.abs(M-sum), sum});
+                    else
+                        break;
+
+                }
+            }
+        }
+
+        Collections.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]-o2[0];
+            }
+        });
+
+        System.out.println(list.get(0)[1]);
+
+
+
+    }
+
+
+}
